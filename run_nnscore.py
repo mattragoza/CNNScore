858fc71509011894510ef537be03681e7fdffe0a
mattragoza <mtr22@pitt.edu>
2015-09-29 20:48:39 -0400

    one aggregate logfile per run

--- run_nnscore.py
+++ run_nnscore.py
@@ -3,9 +3,10 @@ import datetime
 
 if __name__ == "__main__":
 
+	timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 	for i in range(10):
 
 		os.system("caffe train \
 			--model=model/NNScore/nnscore_model_" + str(i) + ".prototxt \
 			--solver=solver/NNScore/nnscore_solver_" + str(i) + ".prototxt \
-			2>&1 | tee logs/nnscore_" + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") +".log")
+			2>&1 | tee -a logs/nnscore_" + timestamp +".log")
