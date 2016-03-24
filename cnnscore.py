e83e41be1e23f5592348cf028e780e56ed1eaee5
mattragoza <mtr22@pitt.edu>
2016-03-24 14:23:02 -0400

    add helper functions for dataviz, changed pop to del in write_model_prototxt

--- cnnscore.py
+++ cnnscore.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use('Agg')
 import sys
 import os
+import re
 import argparse
 import numpy as np
 
@@ -14,7 +15,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as colormap
 
 
-DEFAULT_BINMAP_ROOT = '/scr/DUDe/'
+DEFAULT_BINMAP_ROOT = '/scr/CSAR/'
 DEFAULT_OUTPUT_DIR = './'
 DEFAULT_GPUS = '0'
 
@@ -72,6 +73,44 @@ def read_data_to_field_dict(data_file, types, fields):
     return data
 
 
+def parse_log_file(log_file, period=2):
+
+    loss_data = dict(iter=[], loss=[])
+    i, loss = 0, 0
+    for ln, line in enumerate(read_lines_from_file(log_file)):
+        if len(line) > 4 and line[4] == 'Iteration':
+            i = int(line[5].strip(','))
+            loss += float(line[8])
+            if (ln%period)+1 == period:
+                loss_data['iter'].append(i)
+                loss_data['loss'].append(loss/period)
+                i, loss = 0, 0
+
+    return loss_data
+
+
+def plot_training_progress(train_plot_file, loss_data, tot_auroc_data, cv_auroc_data):
+
+    plt.clf()
+    plt.figure(figsize=(6, 4))
+    plt.title('Training Loss and AUROC')
+    plt.xlabel('Iteration')
+    plt.ylabel('Percent')
+
+    colors = colormap.rainbow(np.linspace(0, 1, 3))
+
+    plt.plot(loss_data['iter'], loss_data['loss'], '-', color=colors[0], linewidth=2, \
+            label='Training Loss')
+    plt.plot(tot_auroc_data['iter'], tot_auroc_data['auroc'], '-', color=colors[1], linewidth=2, \
+            label='Train set AUROC')
+    plt.plot(cv_auroc_data['iter'], cv_auroc_data['auroc'], '-', color=colors[2], linewidth=2, \
+            label='Test set AUROC')
+
+    legend = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.savefig(train_plot_file, bbox_extra_artists=(legend,), bbox_inches='tight')
+
+
+
 def reduce_data(data, factor):
     '''Creates a reduced data dict by randomly sampling from each target
     in the given dictionary such that each targets's examples have been
@@ -116,23 +155,62 @@ def write_target_data_to_file(data_file, data, targets=None):
         f.write('\n'.join(lines) + '\n')
 
 
-def plot_roc_curves(roc_plot_file, plot_data):
+def write_k_fold_data(full_data_file, k):
+
+    data_dir = os.path.dirname(full_data_file)
+    data_param, data_ext = os.path.splitext(os.path.basename(full_data_file))
+    full_data = read_data_to_target_dict(full_data_file, [int, str, str], 1)
+    parts = k_fold_partitions(full_data, k)
+    for p, part in enumerate(parts):
+
+        part_param = 'part' + str(p+1)
+        train_data_file = os.path.join(data_dir, '_'.join([data_param, part_param, 'train']) + data_ext)
+        test_data_file  = os.path.join(data_dir, '_'.join([data_param, part_param, 'test'])  + data_ext)    
+        train_targets = [t for t in full_data if t not in part]
+        test_targets = part
+        write_target_data_to_file(train_data_file, full_data, train_targets)
+        write_target_data_to_file(test_data_file, full_data, test_targets)
+
+
+def get_vina_roc_data(data_file):
+
+    data = read_data_to_field_dict(data_file, [int, str, str, str, float, float],
+        ['labels', 'targets', 'examples', None, None, 'vina'])
+    data['vina'] = [-i for i in data['vina']]
+    fpr, tpr, thresholds = roc_curve(data['labels'], data['vina'])
+    return dict(fpr=fpr, tpr=tpr, auroc=auc(fpr, tpr))
+
+
+def plot_roc_curves(roc_plot_file, plot_data, vina=None):
 
     plt.clf()
+    plt.figure(figsize=(5, 5))
     plt.title('Receiver Operating Characteristic')
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='random guess')
     
-    colors = colormap.rainbow(np.linspace(0, 1, len(plot_data)))
+    auroc_data = []
+    num_colors = len(plot_data)
+    if vina: num_colors += 1
+    colors = colormap.rainbow(np.linspace(0, 1, num_colors))
+
+    if vina:
+        plt.plot(vina['fpr'], vina['tpr'], '-', color=colors[-1], linewidth=2, \
+            label='AutoDock Vina' + ' (AUC=%0.2f)' % vina['auroc'])
+
     for i, series in enumerate(plot_data):
 
-        fpr, tpr, thresholds = roc_curve(series['labels'], series['scores']) 
-        plt.plot(fpr, tpr, '-', color=colors[i], \
-            label=series.get('name', 'series %d' % i) + ' (AUC=%0.2f)' % auc(fpr, tpr))
+        fpr, tpr, thresholds = roc_curve(series['labels'], series['scores'])
+        auroc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, '-', color=colors[i], linewidth=2, \
+            label=series.get('name', 'series %d' % i) + ' (AUC=%0.2f)' % auroc)
+        auroc_data.append(auroc)
 
     legend = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     plt.savefig(roc_plot_file, bbox_extra_artists=(legend,), bbox_inches='tight')
+
+    return auroc_data
 
 
 def write_scores_to_file(score_file, results):
@@ -190,19 +268,16 @@ def write_model_prototxt(model_file, model_prototype, data_file, binmap_root, mo
     data_layer.ndim_data_param.root_folder = binmap_root
 
     if mode == 'train':
-        data_layer.ndim_data_param.shuffle = True
-        data_layer.ndim_data_param.balanced = True
-        data_layer.ndim_data_param.rotate = 10
         for i, layer in enumerate(model.layer):
             if layer.name == 'pred':
-                model.layer.pop(i)
-
+                del model.layer[i]
+    
     elif mode == 'test':
         data_layer.ndim_data_param.shuffle = False
         data_layer.ndim_data_param.balanced = False
         for i, layer in enumerate(model.layer):
             if layer.name == 'loss':
-                model.layer.pop(i)
+                del model.layer[i]
 
     with open(model_file, 'w') as f:
         f.write(str(model))
@@ -237,17 +312,24 @@ def read_solver_prototxt(solver_file):
     return solver
 
 
-def generate_crossval_files(output_dir, full_data_file, binmap_root, model_prototype_file, solver_prototype_file, k=0):
+def generate_crossval_files(output_dir, full_data_file, binmap_root, model_prototype_file, solver_prototype_file, k=3, write=False):
 
     # keep track of names of all generated files in a dictionary
     crossval_files = dict(train_data=[], test_data=[], train_models=[], test_models=[], \
-        solvers=[], weights=[], scores=[], roc_plots=dict(by_part=[], by_iter=[]))
+        solvers=[], weights=[], scores=[], roc_plots=dict(crossval=None, testontrain=None, by_iter=[]), line_plot=None)
 
     # get parameter strings for data, model, and solver arguments
     data_dir = os.path.dirname(full_data_file)
     data_param, data_ext = os.path.splitext(os.path.basename(full_data_file))
     model_param  = os.path.basename(model_prototype_file).split('.')[0] # model_param.model.prototxt
     solver_param = os.path.basename(solver_prototype_file).split('.')[0] # solver_param.solver.prototxt
+
+    model_prototype  = read_model_prototxt(model_prototype_file)
+    solver_prototype = read_solver_prototxt(solver_prototype_file)
+    max_iter = solver_prototype.max_iter
+    snap_iter = solver_prototype.snapshot
+    if not snap_iter:
+        snap_iter = max_iter
 
     for i in range(k+1):
 
@@ -264,17 +346,15 @@ def generate_crossval_files(output_dir, full_data_file, binmap_root, model_proto
         crossval_files['test_data'].append(test_data_file)
 
 
-        # create prototxt for train model, test model and solver
-        model_prototype  = read_model_prototxt(model_prototype_file)
-        solver_prototype = read_solver_prototxt(solver_prototype_file)
-        
+        # prototxt for train model, test model and solver   
         train_model_file = os.path.join(output_dir, '_'.join([model_param, part_param, 'train']) + '.model.prototxt')
         test_model_file  = os.path.join(output_dir, '_'.join([model_param, part_param, 'test'])  + '.model.prototxt')
         solver_file = os.path.join(output_dir, '_'.join([solver_param, part_param]) + '.solver.prototxt')
         
-        write_model_prototxt(train_model_file, model_prototype, train_data_file, binmap_root, mode='train')
-        write_model_prototxt(test_model_file,  model_prototype, test_data_file,  binmap_root, mode='test')
-        write_solver_prototxt(solver_file, solver_prototype, train_model_file)
+        if write:
+            write_model_prototxt(train_model_file, model_prototype, train_data_file, binmap_root, mode='train')
+            write_model_prototxt(test_model_file,  model_prototype, test_data_file,  binmap_root, mode='test')
+            write_solver_prototxt(solver_file, solver_prototype, train_model_file)
         
         crossval_files['train_models'].append(train_model_file)
         crossval_files['test_models'].append(test_model_file)
@@ -282,11 +362,6 @@ def generate_crossval_files(output_dir, full_data_file, binmap_root, model_proto
 
 
         # keep track of weight files and score files that will be produced from training snapshots
-        max_iter = solver_prototype.max_iter
-        snap_iter = solver_prototype.snapshot
-        if not snap_iter:
-            snap_iter = max_iter
-
         weight_files, score_files = [], []
         for j in range(snap_iter, max_iter+1, snap_iter):
             iter_param = 'iter_' + str(j)
@@ -299,20 +374,25 @@ def generate_crossval_files(output_dir, full_data_file, binmap_root, model_proto
         crossval_files['scores'].append(score_files)
 
 
-        # plot ROC curves by crossval part and by training iteration
-        if not crossval_files['roc_plots']['by_iter']:
-            for j in range(snap_iter, max_iter+1, snap_iter):
-                iter_param = 'iter_' + str(j)
-                roc_plot_file = os.path.join(output_dir, '_'.join([model_param, iter_param]) + '.roc.png')
-                crossval_files['roc_plots']['by_iter'].append(roc_plot_file)
+    # plot ROC curves for test-on-train, crossval, and both by training iteration
+    for j in range(snap_iter, max_iter+1, snap_iter):
+        iter_param = 'iter_' + str(j)
+        roc_plot_file = os.path.join(output_dir, '_'.join([model_param, iter_param]) + '.roc.png')
+        crossval_files['roc_plots']['by_iter'].append(roc_plot_file)
 
-        roc_plot_file = os.path.join(output_dir, '_'.join([model_param, part_param]) + '.roc.png')
-        crossval_files['roc_plots']['by_part'].append(roc_plot_file)
+    roc_plot_file = os.path.join(output_dir, '_'.join([model_param, "testontrain"]) + '.roc.png')
+    crossval_files['roc_plots']['testontrain']= roc_plot_file
+
+    roc_plot_file = os.path.join(output_dir, '_'.join([model_param, "crossval"]) + '.roc.png')
+    crossval_files['roc_plots']['crossval'] = roc_plot_file
+
+    line_plot_file = os.path.join(output_dir, '_'.join([model_param]) + '.line.png')
+    crossval_files['line_plot'] = line_plot_file
 
     return crossval_files
 
 
-def generate_test_files(output_dir, test_data_file, binmap_root, model_prototype_file, weight_file):
+def generate_test_files(output_dir, test_data_file, binmap_root, model_prototype_file, weight_file, write=False):
 
     test_files = dict(test_data=None, test_model=None, score=None)
 
@@ -320,9 +400,11 @@ def generate_test_files(output_dir, test_data_file, binmap_root, model_prototype
     data_param, data_ext = os.path.splitext(os.path.basename(test_data_file)) # data_param.data_ext
     model_param = os.path.basename(model_prototype_file).split('.')[0] # model_param.model.prototxt
 
-    model_prototype = read_model_prototxt(model_prototype_file)
     test_model_file = os.path.join(output_dir, '_'.join([model_param, data_param])  + '.model.prototxt')
-    write_model_prototxt(test_model_file, model_prototype, test_data_file, binmap_root, mode='test')
+    
+    if write:
+        model_prototype = read_model_prototxt(model_prototype_file)
+        write_model_prototxt(test_model_file, model_prototype, test_data_file, binmap_root, mode='test')
 
     weight_param = os.path.splitext(os.path.basename(weight_file))[0]
     score_file = os.path.join(output_dir, '_'.join([weight_param, data_param]) + '.scores')
@@ -334,11 +416,54 @@ def generate_test_files(output_dir, test_data_file, binmap_root, model_prototype
     return test_files
 
 
+def draw_crossval_plots(output_dir, data_file, model_file, solver_file, opts, log_file=None):
+
+    k = 3
+    binmap_root = opts.get('b', DEFAULT_BINMAP_ROOT)
+    crossval_files = generate_crossval_files(output_dir, data_file, binmap_root, model_file, solver_file, k, write=False)
+    
+    auroc_iters = []
+    all_score_data = [[], []] # testontrain, crossval
+    for i in range(k+1):
+
+        for j, weight_file in enumerate(crossval_files['weights'][i]):
+            score_file = crossval_files['scores'][i][j]
+            score_data = read_data_to_field_dict(score_file, types=[int, str, str, float], \
+                                    fields=['labels', 'targets', 'examples', 'scores'])
+            if i == 0:
+                score_data['name'] = "ConvNet, test-on-train"
+                all_score_data[0].append(score_data)
+                auroc_iters.append(int(re.search(r'iter_(\d+)', score_file).group(1)))
+            elif i == 1:
+                score_data['name'] = "ConvNet, cross-validation"
+                all_score_data[1].append(score_data)
+            else:
+                all_score_data[1][j]['labels'] += score_data['labels']
+                all_score_data[1][j]['targets'] += score_data['targets']
+                all_score_data[1][j]['examples'] += score_data['examples']
+                all_score_data[1][j]['scores'] += score_data['scores']
+
+    tot_aurocs = plot_roc_curves(crossval_files['roc_plots']['testontrain'], all_score_data[0])
+    cv_aurocs = plot_roc_curves(crossval_files['roc_plots']['crossval'], all_score_data[1])
+    if log_file:
+        loss_data = parse_log_file(log_file)
+        tot_auroc_data = dict(iter=[0]+auroc_iters, auroc=[0.5]+tot_aurocs)
+        cv_auroc_data = dict(iter=[0]+auroc_iters, auroc=[0.5]+cv_aurocs)
+        train_plot_file = crossval_files['roc_plots']['crossval'].replace('_crossval.roc.png', '.train_progress.png')
+        plot_training_progress(train_plot_file, loss_data, tot_auroc_data, cv_auroc_data)
+
+    vina_roc_data = get_vina_roc_data(data_file)
+    by_iter_score_data = transpose(all_score_data)
+    for i, roc_plot_file in enumerate(crossval_files['roc_plots']['by_iter']):
+        plot_roc_curves(roc_plot_file, by_iter_score_data[i], vina=vina_roc_data)
+
+
+
 def crossval_model(output_dir, data_file, model_file, solver_file, opts):
 
     k = 3
     binmap_root = opts.b or DEFAULT_BINMAP_ROOT
-    crossval_files = generate_crossval_files(output_dir, data_file, binmap_root, model_file, solver_file, k)
+    crossval_files = generate_crossval_files(output_dir, data_file, binmap_root, model_file, solver_file, k, write=True)
     
     gpus = opts.g or DEFAULT_GPUS
     for i in range(k+1):
@@ -347,32 +472,41 @@ def crossval_model(output_dir, data_file, model_file, solver_file, opts):
 
     caffe.set_device(int(gpus.split(',')[0])) # can pycaffe do multi-gpu?
     caffe.set_mode_gpu()
-    all_plot_data = []
+    
+    all_score_data = [[], []] # testontrain, crossval
     for i in range(k+1):
         test_data_file = crossval_files['test_data'][i]
         test_model_file = crossval_files['test_models'][i]
 
-        part_plot_data = []
         for j, weight_file in enumerate(crossval_files['weights'][i]):
             score_file = crossval_files['scores'][i][j]
             score_data = get_model_predictions(test_model_file, weight_file, test_data_file)
-            score_data['name'] = os.path.basename(score_file).replace('.scores', '')
             write_scores_to_file(score_file, score_data)
-            part_plot_data.append(score_data)
 
-        all_plot_data.append(part_plot_data)
+            if i == 0:
+                score_data['name'] = os.path.basename(score_file)[:-7].replace("full", "testontrain")
+                all_score_data[0].append(score_data)
+            elif i == 1:
+                score_data['name'] = os.path.basename(score_file)[:-7].replace("part1", "crossval")
+                all_score_data[1].append(score_data)
+            else:
+                all_score_data[1][j]['labels'] += score_data['labels']
+                all_score_data[1][j]['targets'] += score_data['targets']
+                all_score_data[1][j]['examples'] += score_data['examples']
+                all_score_data[1][j]['scores'] += score_data['scores']
 
-    for i, roc_plot_file in enumerate(crossval_files['roc_plots']['by_part']):
-        plot_roc_curves(roc_plot_file, all_plot_data[i])
+    plot_roc_curves(crossval_files['roc_plots']['testontrain'], all_score_data[0])
+    plot_roc_curves(crossval_files['roc_plots']['crossval'], all_score_data[1])
 
+    by_iter_score_data = transpose(all_score_data)
     for i, roc_plot_file in enumerate(crossval_files['roc_plots']['by_iter']):
-        plot_roc_curves(roc_plot_file, transpose(all_plot_data)[i])
+        plot_roc_curves(roc_plot_file, by_iter_score_data[i])
 
 
 def test_model(output_dir, data_file, model_file, weight_file, opts):
 
     binmap_root = opts.b or DEFAULT_BINMAP_ROOT
-    test_files = generate_test_files(output_dir, data_file, binmap_root, model_file, weight_file)
+    test_files = generate_test_files(output_dir, data_file, binmap_root, model_file, weight_file, write=True)
     
     gpus = opts.g or DEFAULT_GPUS
     caffe.set_device(int(gpus.split(',')[0])) 
